0af28d802597652dda15baee589cc045a9494019
Crazymaniac781 <seebednar@gmail.com>
2022-11-18 15:53:27 -0600

    Test Code

---
+++ test_account.py
@@ -0,0 +1,18 @@
+from account import account
+
+def test_init():
+    acc1 = account("Uma")
+    assert acc1.getname() == "Uma"
+    assert acc1.getbalance() == 0
+
+def test_deposit():
+    acc1 = account("Uma")
+    acc1.deposit(20)
+    assert acc1.getbalance() == 20
+
+
+def test_withdraw():
+    acc1 = account("Uma")
+    acc1.deposit(100)
+    acc1.withdraw(20)
+    assert acc1.getbalance() == 80
