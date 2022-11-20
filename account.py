43da2ddcbc8645564177b6c4805d397228920f88
Crazymaniac781 <seebednar@gmail.com>
2022-11-20 12:50:29 -0600

    Test Code

--- account.py
+++ account.py
@@ -1,17 +1,32 @@
-class account:
-    def __init__(self, name):
+class Account:
+    """
+    account class is to create  objects for an account with some basic methods deposit , withdraw , getbalance and getname
+    """
+    def __init__(self, name: str):
+        """
+        Constructor to initialise the objects of account class
+        :param name: The Person's name
+        """
         self.__account_name = name
         self.__account_balance = 0
-
-    def deposit(self, amount):
+    def deposit(self, amount: int) -> bool:
+        """
+        deposit(amount) method deposits or add specified amount to the account balance
+        :param amount: How much the person is depositing
+        :return: Checking to see if they are adding more than 0 then return True or False
+        """
         if amount > 0:
             self.__account_balance = self.__account_balance + amount
             return True
-
         else:
             return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: int) -> bool:
+        """
+        withdraw(amount) method to withdraw the specified amount from the account balance
+        :param amount: How much they are withdrawing
+        :return: Checking to see if they have enough money to withdraw said amount and if the amount is more than 0
+        """
         if amount > 0:
             if amount <= self.__account_balance:
                 self.__account_balance = self.__account_balance - amount
@@ -20,9 +35,15 @@ class account:
                 return False
         else:
             return False
-
-    def getbalance(self):
+    def getbalance(self) -> int:
+        """
+        getter method to get the private variable __account_balance alias bank balance amount
+        :return: Account balance
+        """
         return self.__account_balance
-
-    def getname(self):
+    def getname(self) -> str:
+        """
+        getter method to get the private variable __account_name alias bank holder name
+        :return: Name associated with the account
+        """
         return self.__account_name
--- test_account.py
+++ test_account.py
@@ -1,18 +1,31 @@
-from account import account
+from account import *
+class Test:
 
-def test_init():
-    acc1 = account("Uma")
-    assert acc1.getname() == "Uma"
-    assert acc1.getbalance() == 0
+    def setup_method(self):
+        self.a1 = Account('John')
+        self.a2 = Account('Jane')
 
-def test_deposit():
-    acc1 = account("Uma")
-    acc1.deposit(20)
-    assert acc1.getbalance() == 20
+    def test_init(self):
+        assert self.a1.getname() == 'John'
+        assert self.a1.getbalance() == 0
+        assert self.a2.getname() == 'Jane'
+        assert self.a2.getbalance() == 0
 
+    def test_deposit(self):
+        assert self.a1.deposit(-2) is False
+        assert self.a1.getbalance() == 0
+        self.a1.deposit(0)
+        assert self.a1.getbalance() == 0
+        self.a1.deposit(1)
+        assert self.a1.getbalance() == 1
 
-def test_withdraw():
-    acc1 = account("Uma")
-    acc1.deposit(100)
-    acc1.withdraw(20)
-    assert acc1.getbalance() == 80
+    def test_withdraw(self):
+        assert self.a2.withdraw(-2) is False
+        assert self.a2.withdraw(2) is False
+        assert self.a2.withdraw(0) is False
+        self.a2.deposit(5)
+        assert self.a2.withdraw(2) == True
+
+    def tear_down(self):
+        del self.a1
+        del self.a2
